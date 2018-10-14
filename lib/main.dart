b2a1ab4fac4430063a550d0dd73fcec7f5dd1be8
alhanouf <alhanoufaddawood@gmail.com>
2018-10-14 17:22:43 +0300

    before signup auth

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import './pages/auth.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
 import './pages/product.dart';
-import './models/product.dart';
 import './scoped-models/main.dart';
 
 void main() {
