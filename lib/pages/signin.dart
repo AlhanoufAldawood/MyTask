6dbe5dbe9a4c07c849dcc08c6d2c1e84f6e8b5be
alhanouf <alhanoufaddawood@gmail.com>
2018-10-14 19:37:45 +0300

    Login DONE :D

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -11,11 +11,10 @@ class signin extends StatefulWidget {
 }
 
 class _SigninState extends State<signin> {
-
-      final Map<String, dynamic> _formData = {
-        'email': null,
-        'password': null,
-        'acceptTerms': false
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false
   };
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -24,126 +23,125 @@ class _SigninState extends State<signin> {
     return DecorationImage(
       fit: BoxFit.cover,
       colorFilter:
-      ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
       image: AssetImage('assets/Welcome background.png'),
     );
   }
 
   Widget _buildEmailTextField() {
-     return TextFormField(
+    return TextFormField(
       decoration: InputDecoration(
-      labelText: 'E-Mail', filled: true, fillColor: Colors.white),
+          labelText: 'E-Mail', filled: true, fillColor: Colors.white),
       keyboardType: TextInputType.emailAddress,
       validator: (String value) {
-      if (value.isEmpty ||
-  !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-      .hasMatch(value)) {
-    return 'Please enter a valid email';
+        if (value.isEmpty ||
+            !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                .hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (String value) {
+        _formData['email'] = value;
+      },
+    );
   }
-  },
-    onSaved: (String value) {
-       _formData['email'] = value;
-    },
-   );
-  }
-
-
 
   Widget _buildPasswordTextField() {
     return TextFormField(
       decoration: InputDecoration(
-      labelText: 'Password', filled: true, fillColor: Colors.white),
+          labelText: 'Password', filled: true, fillColor: Colors.white),
       obscureText: true,
-     validator: (String value) {
-     if (value.isEmpty || value.length < 6) {
-      return 'Password invalid';
-  }
-  },
-  onSaved: (String value) {
-  _formData['password'] = value;
-  },
-  );
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Password invalid';
+        }
+      },
+      onSaved: (String value) {
+        _formData['password'] = value;
+      },
+    );
   }
 
+  void _submitForm(Function login) async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
 
-   void _submitForm (Function login){
-  if (!_formKey.currentState.validate() ) {
-  return;
-  }
-  _formKey.currentState.save();
-  login(_formData['email'], _formData['password']);
-  Navigator.pushReplacementNamed(context, '/products');
+    _formKey.currentState.save();
+
+    final Map<String, dynamic> successInformation =
+        await login(_formData['email'], _formData['password']);
+
+    if (successInformation['success']) {
+      Navigator.pushReplacementNamed(context, '/products');
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Somthing wrong!!'),
+              content: Text(successInformation['message']),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
 
-       final double deviceWidth = MediaQuery.of(context).size.width;
-       final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
-
-       return Scaffold(
-
-
-         body: Container(
-            decoration: BoxDecoration(
-             image: _buildBackgroundImage(),
-           ),
-
-           padding: EdgeInsets.all(10.0),
-
-           child: Center(
-             child: SingleChildScrollView(
-               child: Container(
-                 width: targetWidth,
-
-                 child: Form(
-                   key: _formKey,
-
-                   child: Column(
-
-                     children: <Widget>[
-
-                       _buildEmailTextField(),
-
-                       SizedBox(
-                         height: 10.0,
-                       ),
-
-  _buildPasswordTextField(),
-
-
-                       SizedBox(
-                         height: 10.0,
-                       ),
-
-    ScopedModelDescendant <MainModel>(builder:(BuildContext context , Widget child , MainModel model) {
-      return
-      RaisedButton(
-          color: Colors.white,
-          textColor: Colors.red[200],
-          child: Text('LOGIN'),
-          onPressed: () => _submitForm(model.login),
-      );
-      } ),
-
-                     ],
-
-                   ),
-
-                 ),
-
-               ),
-
-             ),
-
-           ),
-
-         ),
-
-       );
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: _buildBackgroundImage(),
+        ),
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              width: targetWidth,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    _buildEmailTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _buildPasswordTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ScopedModelDescendant<MainModel>(builder:
+                        (BuildContext context, Widget child, MainModel model) {
+                      return model.isloading
+                          ? CircularProgressIndicator()
+                          : RaisedButton(
+                              color: Colors.white,
+                              textColor: Colors.red[200],
+                              child: Text('SIGN IN'),
+                              onPressed: () => _submitForm(model.login),
+                            );
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
-  }
-
+}
 
 /*
 
--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -5,58 +5,49 @@ import 'dart:async';
 import '../models/user.dart';
 import '../models/product.dart';
 
-
 class ConnectedProductsModel extends Model {
-
   List<Child> _childern = [];
   int _selProductIndex;
   User _authenticatedUser;
   bool _isLoading = false;
 
   Future<Null> addProduct(
-      String name , String gender, double age ,String user ,String password) {
-      _isLoading = true;
-      notifyListeners();
-    final Map<String ,dynamic> childData ={
-      'name' : name,
-      'gender' : gender,
-      'age' : age,
+      String name, String gender, double age, String user, String password) {
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> childData = {
+      'name': name,
+      'gender': gender,
+      'age': age,
       'user': user,
-      'password':password,
-      'parentEmail' : _authenticatedUser.email,
+      'password': password,
+      'parentEmail': _authenticatedUser.email,
       'parentId': _authenticatedUser.id,
-
     };
-     return
-       http.post('https://mytask-8c4b1.firebaseio.com/childern.json' ,
-        body: json.encode(childData)).then((http.Response response) {
-          _isLoading =false;
-          final Map<String ,dynamic> responseData = json.decode(response.body);
+    return http
+        .post('https://mytask-8c4b1.firebaseio.com/childern.json',
+            body: json.encode(childData))
+        .then((http.Response response) {
+      _isLoading = false;
+      final Map<String, dynamic> responseData = json.decode(response.body);
 
-          final Child newchild = Child(
+      final Child newchild = Child(
           id: responseData['name'],
-          name: name ,
-          gender: gender ,
-          age : age,
+          name: name,
+          gender: gender,
+          age: age,
           user: user,
-          password:password ,
-          parentEmail: _authenticatedUser.email ,
-          parentId: _authenticatedUser.id );
+          password: password,
+          parentEmail: _authenticatedUser.email,
+          parentId: _authenticatedUser.id);
 
       _childern.add(newchild);
       notifyListeners();
-
-    }
-    );
-
-
-
+    });
   }
-
 }
 
 class ProductsModel extends ConnectedProductsModel {
-
   List<Child> get allProducts {
     return List.from(_childern);
   }
@@ -72,9 +63,8 @@ class ProductsModel extends ConnectedProductsModel {
     return _childern[selectedProductIndex];
   }
 
-
-
-  Future<Null> updateProduct(String name , String gender, double age ,String user ,String password) {
+  Future<Null> updateProduct(
+      String name, String gender, double age, String user, String password) {
     _isLoading = true;
     notifyListeners();
     final Map<String, dynamic> updateData = {
@@ -85,12 +75,11 @@ class ProductsModel extends ConnectedProductsModel {
       'password': password,
       'parentEmail': _authenticatedUser.email,
       'parentId': _authenticatedUser.id,
-
     };
-    return
-      http.put('https://mytask-8c4b1.firebaseio.com/childern/${selectedProduct
-        .id}.json',
-        body: json.encode(updateData))
+    return http
+        .put(
+            'https://mytask-8c4b1.firebaseio.com/childern/${selectedProduct.id}.json',
+            body: json.encode(updateData))
         .then((http.Response response) {
       _isLoading = false;
       final Child updatedChild = Child(
@@ -107,10 +96,8 @@ class ProductsModel extends ConnectedProductsModel {
     });
   }
 
-
-
   void deleteProduct() {
-    _isLoading =true;
+    _isLoading = true;
 
     final deletedChildId = selectedProduct.id;
 
@@ -118,44 +105,45 @@ class ProductsModel extends ConnectedProductsModel {
     _selProductIndex = null;
     notifyListeners();
 
-    http.delete('https://mytask-8c4b1.firebaseio.com/childern/${deletedChildId}.json')
-    .then((http.Response response){
+    http
+        .delete(
+            'https://mytask-8c4b1.firebaseio.com/childern/${deletedChildId}.json')
+        .then((http.Response response) {
       _isLoading = false;
       notifyListeners();
     });
-
   }
 
-  void fetchData(){
+  void fetchData() {
     _isLoading = true;
     notifyListeners();
-    http.get('https://mytask-8c4b1.firebaseio.com/childern.json')
-    .then((http.Response response){
-       _isLoading = false;
-        notifyListeners();
-      final List<Child> fetchedChildList= [];
+    http
+        .get('https://mytask-8c4b1.firebaseio.com/childern.json')
+        .then((http.Response response) {
+      _isLoading = false;
+      notifyListeners();
+      final List<Child> fetchedChildList = [];
 
-      final Map<String,dynamic> childListData =
-      json.decode(response.body);
+      final Map<String, dynamic> childListData = json.decode(response.body);
 
-      if(childListData == null){
-        _isLoading =false;
+      if (childListData == null) {
+        _isLoading = false;
         notifyListeners();
         return;
       }
-      childListData.forEach((String childId ,dynamic childData) {
+      childListData.forEach((String childId, dynamic childData) {
         final Child child = Child(
-            id: childId,
-            name: childData['name'],
-            gender: childData['gender'],
-            age: childData['age'],
-            user: childData['user'],
-            password: childData['password'],
-            parentEmail: childData['parentEmail'],
-            parentId: childData['parentId'],);
+          id: childId,
+          name: childData['name'],
+          gender: childData['gender'],
+          age: childData['age'],
+          user: childData['user'],
+          password: childData['password'],
+          parentEmail: childData['parentEmail'],
+          parentId: childData['parentId'],
+        );
 
-           fetchedChildList.add(child);
-
+        fetchedChildList.add(child);
       });
       _childern = fetchedChildList;
       notifyListeners();
@@ -168,47 +156,71 @@ class ProductsModel extends ConnectedProductsModel {
 }
 
 class UserModel extends ConnectedProductsModel {
-
-
-  void login(String email, String password) {
-    _authenticatedUser = User(id: 'Alhanouf', email: email, password: password);
-  }
-
-  Future<Map <String, dynamic>> signUp(String email, String password) async {
+  Future<Map<String, dynamic>> login(String email, String password) async {
     _isLoading = true;
     notifyListeners();
-    final Map<String, dynamic> authData = {
 
+    final Map<String, dynamic> authData = {
       'email': email,
       'password': password,
       'returnSecureToken': true,
     };
     final http.Response response = await http.post(
-      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAzUK3qTHibEFZiH5TYLkQNCqvhxjlaAz4',
-      body: json.encode(authData),
-      headers: {'Content-Type': 'application/json'}
-      );
-    final Map<String , dynamic> responseData = json.decode(response.body);
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAzUK3qTHibEFZiH5TYLkQNCqvhxjlaAz4',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'});
+
+    final Map<String, dynamic> responseData = json.decode(response.body);
 
     bool hasError = true;
-    String message ='Somthing went wrong';
+    String message = 'Somthing went wrong';
 
-    if (responseData.containsKey('idToken')){
+    if (responseData.containsKey('idToken')) {
       hasError = false;
-      message ='Authenticaton sucecede';
-    } else if (responseData['error']['message'] == 'EMAIL_EXISTS'){
+      message = 'Authenticaton sucecede';
+    } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
+      message = 'Email not found !!';
+    } else if (responseData['error']['message'] == 'INVALID_PASSWORD') {
+      message = 'invalid password !!';
+    }
+
+    _isLoading = false;
+    notifyListeners();
+    return {'success': !hasError, 'message': message};
+  }
+
+  Future<Map<String, dynamic>> signUp(String email, String password) async {
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true,
+    };
+    final http.Response response = await http.post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAzUK3qTHibEFZiH5TYLkQNCqvhxjlaAz4',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'});
+    final Map<String, dynamic> responseData = json.decode(response.body);
+
+    bool hasError = true;
+    String message = 'Somthing went wrong';
+
+    if (responseData.containsKey('idToken')) {
+      hasError = false;
+      message = 'Authenticaton sucecede';
+    } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
       message = 'This email is already exists';
     }
 
     _isLoading = false;
     notifyListeners();
-    return{'success': !hasError , 'message': message};
+    return {'success': !hasError, 'message': message};
   }
 }
 
 class UtilityModel extends ConnectedProductsModel {
-  bool get isloading{
+  bool get isloading {
     return _isLoading;
   }
-
 }
